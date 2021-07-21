98a14c1de3afb8affd3b139a63ff0160b460f208
blaserlab <bradley.blaser@osumc.edu>
2021-07-21 08:01:41 -0400

    Initial commit

---
+++ R/dev.R
@@ -0,0 +1,9 @@
+# project setup
+library(usethis)
+create_project("/workspace/workspace_pipelines/lapalombella_pu")
+use_mit_license("Brad Blaser")
+use_readme_md()
+use_news_md()
+use_git()
+use_data()
+use_data_raw()
---
+++ README.md
@@ -0,0 +1,8 @@
+
+# lapalombella_pu
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of lapalombella_pu is to ...
+
