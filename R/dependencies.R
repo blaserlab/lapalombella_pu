2c98943f15015ac6bf3bb27fb190de1e2700b88c
blaserlab <bradley.blaser@osumc.edu>
2021-08-09 17:13:26 -0400

    first pass analysis

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,29 +1,32 @@
+# libraries-------------------------------------
 # set up the renv and repair with snapshot if needed
 # renv::init()
 # renv::snapshot()
 
-# install into your home directory if you haven't already
-# devtools::install_github("git@github.com:blaserlab/blaseRtools.git")
-
-
 # blaseRtools and additional dependencies you may have to install since they are not recognized by renv::init
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/blaseRtools")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/DESeq2")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/genefilter")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/annotate")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/AnnotationDbi")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/KEGGREST")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/Biostrings")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/geneplotter")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/DoubletFinder")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/Seurat")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/SeuratDisk")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/rrvgo")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/GO.db")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/GOSemSim")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/scater")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/topGO")
-# renv::install("~/R/x86_64-pc-linux-gnu-library/4.1/fastSave")
+# renv::install("/usr/lib/R/site-library/blaseRtools")
+# renv::install("/usr/lib/R/site-library/DESeq2")
+# renv::install("/usr/lib/R/site-library/genefilter")
+# renv::install("/usr/lib/R/site-library/annotate")
+# renv::install("/usr/lib/R/site-library/AnnotationDbi")
+# renv::install("/usr/lib/R/site-library/KEGGREST")
+# renv::install("/usr/lib/R/site-library/Biostrings")
+# renv::install("/usr/lib/R/site-library/geneplotter")
+# renv::install("/usr/lib/R/site-library/DoubletFinder")
+# renv::install("/usr/lib/R/site-library/Seurat")
+# renv::install("/usr/lib/R/site-library/SeuratDisk")
+# renv::install("/usr/lib/R/site-library/rrvgo")
+# renv::install("/usr/lib/R/site-library/GO.db")
+# renv::install("/usr/lib/R/site-library/GOSemSim")
+# renv::install("/usr/lib/R/site-library/scater")
+# renv::install("/usr/lib/R/site-library/topGO")
+# renv::install("/usr/lib/R/site-library/fastSave")
+# renv::install("/usr/lib/R/site-library/fgsea")
+# renv::install("/usr/lib/R/site-library/fastmatch")
+# renv::install("/usr/lib/R/site-library/tinytex")
+# renv::install("/usr/lib/R/site-library/reshape2")
+# renv::install("/usr/lib/R/site-library/pheatmap")
+# renv::install("/usr/lib/R/site-library/Rcpp")
 
 # load core packages for the analysis
 library("blaseRtools")
@@ -41,9 +44,9 @@ library("readxl")
 
 
 # run this to update the data package in renv
-# bb_renv_datapkg("~/network/X/Labs/Blaser/collaborators/lapalombella_pu_network/datapkg")
+bb_renv_datapkg("~/network/X/Labs/Blaser/collaborators/lapalombella_pu_network/datapkg")
 
 
 # load the data set into a hidden environment
-# requireData("lapalombella.pu.datapkg")
+requireData("lapalombella.pu.datapkg")
 
--- R/scratch.R
+++ R/scratch.R
@@ -1,3 +1,2 @@
-colData(cds_final)
-
-bb_var_umap(cds = cds_final, var = "specimen")
+bb_var_umap(cds = cds_main, var = "specimen")
+bb_var_umap(cds = cds_main, var = "leukemia_phenotype") + facet_grid(cols = vars(primary_or_engraftment), rows = vars(tissue))
