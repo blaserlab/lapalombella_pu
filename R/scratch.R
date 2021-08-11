61bd7fa4b3ffb02268fd72328c46802ba2a45d00
blaserlab <bradley.blaser@osumc.edu>
2021-08-11 10:22:29 -0400

    working session with pu and katie

--- R/scratch.R
+++ R/scratch.R
@@ -1,2 +1,27 @@
-bb_var_umap(cds = cds_main, var = "specimen")
-bb_var_umap(cds = cds_main, var = "leukemia_phenotype") + facet_grid(cols = vars(primary_or_engraftment), rows = vars(tissue))
+bb_var_umap(cds = cds_main, var = "specimen", value_to_highlight = "P9")
+bb_var_umap(cds_main, "leukemia_phenotype")
+bb_var_umap(cds_main[, colData(cds_main)$leukemia_phenotype == "AML"], "partition")
+bb_var_umap(cds_main[, colData(cds_main)$leukemia_phenotype == "AML"], "leiden")
+bb_var_umap(cds_main[, colData(cds_main)$leukemia_phenotype %in% c("AML", "No leukemia")], "partition") +
+  facet_grid(rows = vars(tissue), cols = vars(leukemia_phenotype)) +
+  theme(panel.background = element_rect(color = "grey80"))
+
+bb_gene_umap(cds_main[, colData(cds_main)$leukemia_phenotype %in% c("AML", "No leukemia")], c("Cd14", "Ly6a", "Kit", "Cd3e", "Cd19"))
+
+
+
+bb_var_umap(cds = cds_main[, colData(cds_main)$leukemia_phenotype == "AML"], var = "specimen") +
+  facet_grid(cols = vars(primary_or_engraftment), rows = vars(tissue)) +
+  theme(panel.background = element_rect(color = "grey80"))
+
+
+colData(cds_main) %>%
+  as_tibble() %>%
+  # filter(specimen %in% c("P2", "P14_S1303_SPN_AML")) %>%
+  ggplot(mapping = aes(x = log(Size_Factor), color = specimen)) +
+  geom_density()
+
+
+sequencing_metrics
+
+write_csv(sequencing_metrics, str_glue("{tables_out}/sequencing_metrics.csv"))
