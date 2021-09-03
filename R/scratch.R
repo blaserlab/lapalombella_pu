bc05c0786609993dd1694b4b77c39b0a2fe69bf4
blaserlab <bradley.blaser@osumc.edu>
2021-09-03 15:50:14 -0400

    working session with pu; added pu_work directory and gitignore

--- R/scratch.R
+++ R/scratch.R
@@ -1,27 +1,42 @@
-bb_var_umap(cds = cds_main, var = "specimen", value_to_highlight = "P9")
-bb_var_umap(cds_main, "leukemia_phenotype")
-bb_var_umap(cds_main[, colData(cds_main)$leukemia_phenotype == "AML"], "partition")
-bb_var_umap(cds_main[, colData(cds_main)$leukemia_phenotype == "AML"], "leiden")
-bb_var_umap(cds_main[, colData(cds_main)$leukemia_phenotype %in% c("AML", "No leukemia")], "partition", legend_title = "Partitions") +
-  facet_grid(rows = vars(tissue), cols = vars(leukemia_phenotype)) +
-  theme(panel.background = element_rect(color = "grey80"))
-
-bb_gene_umap(cds_main[, colData(cds_main)$leukemia_phenotype %in% c("AML", "No leukemia")], c("Cd14", "Ly6a", "Kit", "Cd3e", "Cd19"))
-
-
-
-bb_var_umap(cds = cds_main[, colData(cds_main)$leukemia_phenotype == "AML"], var = "specimen") +
-  facet_grid(cols = vars(primary_or_engraftment), rows = vars(tissue)) +
-  theme(panel.background = element_rect(color = "grey80"))
-
+colData(cds_main)
+cds_main
+bb_var_umap(cds_main, "partition", overwrite_labels = T)
+bb_var_umap(cds_main, "leiden")
+bb_var_umap(cds_main, "louvain")
 
 colData(cds_main) %>%
   as_tibble() %>%
-  # filter(specimen %in% c("P2", "P14_S1303_SPN_AML")) %>%
-  ggplot(mapping = aes(x = log(Size_Factor), color = specimen)) +
+  ggplot(mapping = aes(x = log2(Size_Factor), color = specimen)) +
   geom_density()
 
+bb_var_umap(cds_main, "Size_Factor")
+bb_gene_umap(cds_main, "Cd3e")
+bb_gene_umap(cds_main, "Icos")
+bb_gene_umap(cds_main, "Kit")
+bb_gene_umap(cds_main[,colData(cds_main)$partition == "9"], "Kit")
+bb_gene_umap(cds_main[,colData(cds_main)$partition == "9"], "Hoxa9")
+bb_gene_umap(cds_main[,colData(cds_main)$partition == "9"], "Mpo")
+bb_gene_umap(cds_main[,colData(cds_main)$partition == "9"], "Spi1")
+bb_gene_umap(cds_main[,colData(cds_main)$partition == "9"], "Cebpa")
+bb_gene_umap(cds_main[,colData(cds_main)$partition == "9"], "Myb")
+bb_gene_umap(cds_main[,colData(cds_main)$partition == "9"], "Runx1")
+bb_gene_umap(cds = cds_main[,colData(cds_main)$partition == "9"], gene_or_genes = "Ly6a")
 
-sequencing_metrics
 
-write_csv(sequencing_metrics, str_glue("{tables_out}/sequencing_metrics.csv"))
+prealignment_top_markers %>% as_tibble()
+
+
+partition_top_markers <- top_markers(cds_main, group_cells_by = "partition", genes_to_test_per_group = 50, reference_cells = 1000, cores = 39)
+
+prealignment_top_markers <- read_csv("/workspace/workspace_pipelines/lapalombella.pu.datapkg/data/prealignment_top_markers.csv")
+prealignment_top_markers %>%
+  filter(cluster_method == "partition") %>%
+  View()
+
+# partition 9 looks like myeloid
+bb_var_umap(cds_main[,colData(cds_main)$partition == 9], "leiden", overwrite_labels = T)
+
+prealignment_top_markers %>%
+  filter(cell_group %in% c("leiden 16", "leiden 36", "leiden 17", "leiden 22", "leiden 41", "leiden 27", "leiden 33") ) %>%
+  arrange(cell_group) %>%
+  View()
