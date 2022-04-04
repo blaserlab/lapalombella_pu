25e194c5135c44703900685d0c77ab486ffa7f33
pxz122 <pu.zhang@osumc.edu>
2022-04-04 11:27:53 -0400

    fixed git issue

---
+++ R/Pseudotime trajectory-20220401.R
@@ -0,0 +1,240 @@
+#wt-aml-marrow
+colData(cds_wt_aml_marrow)
+cds <- reduce_dimension(cds_wt_aml_marrow)# not so sure why you want to recalculate dimensions
+plot_cells(cds, label_groups_by_cluster=FALSE,  color_cells_by = "leiden_assignment")
+
+ciliated_genes <- c("Cd34",
+                    "Mpo",
+                    "Jun",
+                    "Tox",
+                    "Elane",
+                    "Cd3e")
+
+plot_cells(cds,
+           genes=ciliated_genes,
+           label_cell_groups=FALSE,
+           show_trajectory_graph=FALSE)
+cds <- cluster_cells(cds)# also not sure why you are reclustering
+plot_cells(cds, color_cells_by = "partition")
+
+
+cds <- learn_graph(cds)
+plot_cells(cds,
+           color_cells_by = "leiden_assignment",
+           label_groups_by_cluster=FALSE,
+           label_leaves=FALSE,
+           label_branch_points=FALSE)
+
+cds <- order_cells(cds)
+plot_cells(cds,
+           color_cells_by = "pseudotime",
+           label_cell_groups=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=TRUE,
+           graph_label_size=1.5)
+
+#P53 T ALL
+colData(cds_p53ALL)
+cds <- reduce_dimension(cds_p53ALL)
+plot_cells(cds, label_groups_by_cluster=FALSE,  color_cells_by = "leiden")
+ciliated_genes <- c("Cd34",
+                    "Mpo",
+                    "Jun",
+                    "Tox",
+                    "Elane",
+                    "Cd3e")
+plot_cells(cds,
+           genes=ciliated_genes,
+           label_cell_groups=FALSE,
+           show_trajectory_graph=FALSE)
+cds <- cluster_cells(cds)
+plot_cells(cds, color_cells_by = "partition")
+
+cds <- learn_graph(cds)
+plot_cells(cds,
+           color_cells_by = "leiden",
+           label_groups_by_cluster=FALSE,
+           label_leaves=FALSE,
+           label_branch_points=FALSE)
+
+cds <- order_cells(cds)
+plot_cells(cds,
+           color_cells_by = "pseudotime",
+           label_cell_groups=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=TRUE,
+           graph_label_size=1.5)
+
+#PreB ALL
+colData(cds_preBALL)
+cds <- reduce_dimension(cds_preBALL)
+plot_cells(cds, label_groups_by_cluster=FALSE,  color_cells_by = "leiden")
+ciliated_genes <- c("Cd34",
+                    "Mpo",
+                    "Jun",
+                    "Tox",
+                    "Elane",
+                    "Cd3e")
+plot_cells(cds,
+           genes=ciliated_genes,
+           label_cell_groups=FALSE,
+           show_trajectory_graph=TRUE)
+cds <- cluster_cells(cds)
+plot_cells(cds, color_cells_by = "partition")
+
+cds <- learn_graph(cds)
+plot_cells(cds,
+           color_cells_by = "leiden",
+           label_groups_by_cluster=FALSE,
+           label_leaves=FALSE,
+           label_branch_points=FALSE)
+
+cds <- order_cells(cds)
+plot_cells(cds,
+           color_cells_by = "pseudotime",
+           label_cell_groups=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=TRUE,
+           graph_label_size=1.5)
+
+#Spleen
+colData(cds_spleen)
+cds <- reduce_dimension(cds_spleen)
+plot_cells(cds, label_groups_by_cluster=FALSE,  color_cells_by = "leiden")
+ciliated_genes <- c("Cd34",
+                    "Mpo",
+                    "Jun",
+                    "Tox",
+                    "Elane",
+                    "Cd3e")
+plot_cells(cds,
+           genes=ciliated_genes,
+           label_cell_groups=FALSE,
+           show_trajectory_graph=TRUE)
+cds <- cluster_cells(cds)
+plot_cells(cds, color_cells_by = "partition")
+
+cds <- learn_graph(cds)
+plot_cells(cds,
+           color_cells_by = "leiden",
+           label_groups_by_cluster=FALSE,
+           label_leaves=FALSE,
+           label_branch_points=FALSE)
+
+cds <- order_cells(cds)
+plot_cells(cds,
+           color_cells_by = "pseudotime",
+           label_cell_groups=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=TRUE,
+           graph_label_size=1.5)
+
+
+#cds_wt-aml-spleen
+colData(cds_wt_aml_spleen)
+cds <- reduce_dimension(cds_wt_aml_spleen, reduction_method = c("UMAP"))
+plot_cells(cds, label_groups_by_cluster=FALSE,  color_cells_by = "leiden")
+ciliated_genes <- c("Cd34",
+                    "Mpo",
+                    "Jun",
+                    "Tox",
+                    "Elane",
+                    "Cd3e")
+
+plot_cells(cds,
+           genes=ciliated_genes,
+           label_cell_groups=FALSE,
+           show_trajectory_graph=TRUE)
+cds <- cluster_cells(cds)
+plot_cells(cds, color_cells_by = "partition")
+
+cds <- learn_graph(cds)
+plot_cell_trajectory(cds,
+           color_cells_by = "partition",
+           label_groups_by_cluster=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=FALSE)
+
+
+cds <- order_cells(cds)
+plot_cells(cds,
+           color_cells_by = "pseudotime",
+           label_cell_groups=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=TRUE,
+           graph_label_size=1.5)
+
+genes <- c("Ybx1")
+
+colData(cds)
+rowData(cds)
+lineage_cds <- cds[rowData(cds)$gene_short_name %in% genes,
+                       colData(cds)$leukemia_phenotype %in% c("AML")]
+
+plot_genes_in_pseudotime(lineage_cds,
+                         color_cells_by="leiden",
+                         min_expr=0.5)
+cds_subset <- choose_cells(cds)
+
+
+
+#cds_aml_all
+colData(cds_aml_all)
+cds <- reduce_dimension(cds_aml_all)
+colData(cds)
+plot_cells(cds, label_groups_by_cluster=FALSE,  color_cells_by = "sample")
+
+cds_aml_p5 <- cds_main[,colData(cds_main)$specimen %in% c("P5")]
+cds <- reduce_dimension(cds_aml_p5)
+plot_cells(cds, label_groups_by_cluster=FALSE,  color_cells_by = "leiden")
+
+
+
+ciliated_genes <- c("Cd34",
+                    "Mpo",
+                    "Ezh2",
+                    "Tox",
+                    "Elane",
+                    "Cd3e")
+
+plot_cells(cds,
+           genes=ciliated_genes,
+           label_cell_groups=FALSE,
+           show_trajectory_graph=TRUE)
+cds <- cluster_cells(cds)
+plot_cells(cds, color_cells_by = "partition")
+
+cds <- learn_graph(cds)
+plot_cells(cds,
+                     color_cells_by = "leiden",
+                     label_groups_by_cluster=FALSE,
+                     label_leaves=TRUE,
+                     label_branch_points=FALSE)
+
+
+cds <- order_cells(cds)
+plot_cells(cds,
+           color_cells_by = "pseudotime",
+           label_cell_groups=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=TRUE,
+           graph_label_size=1.5)
+
+genes <- c("Ybx1", "Ezh2", "Elane")
+
+colData(cds)
+rowData(cds)
+lineage_cds <- cds[rowData(cds)$gene_short_name %in% genes,
+                   colData(cds)$leukemia_phenotype %in% c("AML")]
+
+plot_genes_in_pseudotime(lineage_cds,
+                         color_cells_by="leiden",
+                         min_expr=0.5)
+
+plot_cells(cds_p568,
+           color_cells_by = "pseudotime",
+           label_cell_groups=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=TRUE,
+           graph_label_size=1.5)
+cds <- learn_graph(cds_p568)
--- R/working_session_04012022.R
+++ R/working_session_04012022.R
@@ -44,3 +44,98 @@ cds_p568_tm |> filter(cell_group == "louvain 37") |> View()
 bb_gene_dotplot(cds_subset,
                 markers = c("Il7r", "Myb"),
                 group_cells_by = "louvain")
+
+plot_cells(cds_subset,
+           color_cells_by = "louvain",
+           label_groups_by_cluster=FALSE,
+           label_leaves=FALSE,
+           label_branch_points=FALSE)
+
+
+# calculate the differential representation of specimens into partitions (P5 and 8)
+bb_cluster_representation(
+  cds_p568[, colData(cds_p568)$specimen %in% c("P5", "P8")],
+  cluster = "partition",
+  class_var = "specimen",
+  experimental_class = "P5",
+  control_class = "P8",
+  return_value = "table"
+)
+
+cds_subset_1 <- filter_cds(
+  cds = cds_p568,
+  cells = bb_cellmeta(cds_p568) |>
+    filter(specimen %in% c("P5", "P8")) |>
+    filter(partition == "3")
+)
+
+bb_var_umap(cds_subset_1, "louvain", overwrite_labels = T)
+bb_var_umap(cds_subset_1, "density", facet_by = "specimen")
+
+# inspect the top markers
+cds_p568_tm |> filter(cell_group == "louvain 19") |> View()
+cds_p568_tm |> filter(cell_group == "louvain 26") |> View()
+
+
+# visualze selected top markers
+bb_gene_dotplot(cds_subset,
+                markers = c("Il7r", "Myb"),
+                group_cells_by = "louvain")
+
+plot_cells(cds_subset,
+           color_cells_by = "louvain",
+           label_groups_by_cluster=FALSE,
+           label_leaves=FALSE,
+           label_branch_points=FALSE)
+
+# visualze selected top markers
+bb_gene_dotplot(cds_subset_1,
+                markers = c("Klf4", "Itga4", "Fcgr4", "Flt3"),
+                group_cells_by = "louvain")
+
+plot_cells(cds_subset,
+           color_cells_by = "louvain",
+           label_groups_by_cluster=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=FALSE,cell_size = 1,
+           trajectory_graph_segment_size = 0.5,  graph_label_size = 2)
+
+
+cds_subset <- order_cells(cds_subset)
+plot_cells(cds_subset,
+           color_cells_by = "pseudotime",
+           label_cell_groups=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=TRUE,
+           graph_label_size=1.5,cell_size = 1)
+
+cds_subset_1 <- order_cells(cds_subset_1)
+plot_cells(cds_subset_1,
+           color_cells_by = "pseudotime",
+           label_cell_groups=FALSE,
+           label_leaves=TRUE,
+           label_branch_points=TRUE,
+           graph_label_size=1.5,cell_size = 1)
+
+genes_p568_Ball <- c("Il7r", "Myb")
+lineage_cds_p568_Ball <- cds_subset[rowData(cds_subset)$gene_short_name %in% genes_p568_Ball,
+                   colData(cds_subset)$specimen %in% c("P6", "P8")]
+
+plot_genes_in_pseudotime(lineage_cds_p568_Ball,
+                         color_cells_by="louvain",
+                         min_expr=0.5)
+
+genes_p568_aml <- c("Flt3", "Klf4", "Gata2", "Spi1")
+lineage_cds_p568_aml <- cds_subset_1[rowData(cds_subset_1)$gene_short_name %in% genes_p568_aml,
+                          colData(cds_subset_1)$specimen %in% c("P5", "P8")]
+
+plot_genes_in_pseudotime(lineage_cds_p568_aml,
+                         color_cells_by="louvain",
+                         min_expr=0.5)
+
+cds_p568ann<-bb_cds_anno(query_cds=cds_p568, ref=cds_wt_aml_marrow,transfer_col="leiden_assignment", unique_id = NULL )
+
+colData(cds_p568ann)
+bb_var_umap(cds_p568ann, "predicted.leiden_assignment")
+bb_var_umap(cds_p568ann, "leukemia_phenotype")
+
