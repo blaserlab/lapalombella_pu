7cfff8817b0147cea767368e6dd6c01f3e39bc61
EW7721 <ethan.whipp@osumc.edu>
2023-01-18 11:59:17 -0500

    this commit rewinds history to 6ffec25626bb34778ad21846db4cb3bbc2c2f9ed using git revert

--- Figure_5_Composition.R
+++ Figure_5_Composition.R
@@ -3,30 +3,30 @@ T_Figs <- "~/network/T/Labs/EHL/Rosa/Ethan/10X/Tet2_P53/Figures"
 
 # F1top <- plot_grid(NULL, labels = "A")
 
-F6row1 <- plot_grid(
-  NULL,
+F5row1 <- plot_grid(
+  F5A,
   NULL,
   ncol = 2,
-  rel_widths = c(1.5, 1),
+  rel_widths = c(1.75, 1),
   labels = c("A", "B")
 )
 
-F6row2 <- plot_grid(
-  NULL, labels = c("C")
+F5row2 <- plot_grid(
+  aml_gexp_umap, labels = c("C")
 )
-F6row3 <- plot_grid(F6heatmap, NULL, ncol=2, rel_widths = c(0.9,1))#, labels = c("D","E"))
+F5row3 <- plot_grid(F5hm, NULL, ncol=2, rel_widths = c(1,1), labels = "D")
 
-F6hm <- plot_grid(F6row1,
-                       F6row2,
-                       F6row3,
+F5 <- plot_grid(F5row1,
+                       F5row2,
+                       F5row3,
                        nrow = 3,
-                       rel_heights = c(0.75, 1, 1.5)) #0.75,1.2,1
+                       rel_heights = c(0.75, 1.2, 1))
 
-save_plot(F6hm,
-          filename = "tempF6.png",
+save_plot(F5,
+          filename = "tempF5.png",
           base_width = 7.5,
           base_height = 9.75)
-#ggsave("F6hm.pdf", path = T_Figs, width = 7.5, height = 9.75)
+#ggsave("F5.pdf", path = T_Figs, width = 7.5, height = 9.75)
 
 #Supplemental Fig1
 
