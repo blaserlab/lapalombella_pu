8935a7d99555acffa18c78a42ffdf9d973541a51
blaserlab <bradley.blaser@osumc.edu>
2024-12-13 21:02:40 -0500

    extracted hto lookups

---
+++ python/hto.py
@@ -0,0 +1,19 @@
+import scanpy as sc
+import pandas as pd
+from pathlib import Path
+
+input_folder = Path("/home/OSUMC.EDU/blas02/network/X/Labs/Blaser/staff/single_cell/lapalombella_citeseq_dec2024/fastq_pipestance/")
+output_folder = Path("/home/OSUMC.EDU/blas02/network/X/Labs/Blaser/staff/single_cell/lapalombella_citeseq_dec2024/hto_lookup/")
+
+files = [
+    (input_folder / "PZ-3606_WT-pool1_CITE/Hashtag_results/adataFinal_PZ-3606_WT-pool1.h5ad", output_folder / "WT_pool1.csv"),
+    (input_folder / "PZ-3625_WT-pool2_CITE/Hashtag_results/adataFinal_PZ-3625_WT-pool2.h5ad", output_folder / "WT_pool2.csv"),
+    (input_folder / "PZ-3630_COMUTANT-pool2_CITE/Hashtag_results/adataFinal_PZ-3630_COMUTANT-pool2.h5ad", output_folder / "comutant_pool2.csv"),
+    (input_folder / "PZ-3630_TET2-pool2_CITE/Hashtag_results/adataFinal_PZ-3630_TET2-pool2.h5ad", output_folder / "tet2_pool2.csv"),
+    (input_folder / "PZ-3630_TP53-pool2_CITE/Hashtag_results/adataFinal_PZ-3630_TP53-pool2.h5ad", output_folder / "tp53_pool2.csv")
+    ]
+
+for input_file, output_file in files:
+    hto = sc.read_h5ad(input_file)
+    hto_obs = pd.DataFrame(hto.obs)
+    hto_obs.to_csv(output_file, index = False)
