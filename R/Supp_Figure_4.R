99c52199d6818566ce8aae5b88191f355ee51f98
EW7721 <ethan.whipp@osumc.edu>
2025-01-24 14:53:54 -0500

    Supp Fig4 clean

--- R/Supp_Figure_4.R
+++ R/Supp_Figure_4.R
@@ -1,6 +1,11 @@
-#Supp Figure 4
-b_all_plotlist <- map(.x = c("Cd19", "Pax5", "Cd24a", "Cd79a", "Vpreb1", "Vpreb2", "Vpreb3", "Dntt"),
-                    .f = \(x, dat = cds_main) {
+#Supplemental Figure 4
+#Supplemental Figure 4A
+b_plotlist <- map(.x = c("Cd19", "Pax5", "Cd24a", "Cd79a", "Vpreb1", "Vpreb2", "Vpreb3", "Dntt"),
+                    .f = \(x, dat = filter_cds(
+                      cds_main,
+                      cells = bb_cellmeta(cds_main) |>
+                        filter(geno_pheno %in% c("dKO: AML", "Wildtype", "P53 KO: T ALL"))
+                    )) {
                       p <- bb_gene_umap(
                         dat,
                         gene_or_genes = x,
@@ -11,7 +16,7 @@ b_all_plotlist <- map(.x = c("Cd19", "Pax5", "Cd24a", "Cd79a", "Vpreb1", "Vpreb2
                         scale_color_distiller(palette = "Greens",
                                               direction = 1,
                                               na.value = "grey80", limits = c(0,2)) +
-                        facet_wrap(~geno_pheno) + scale_y_continuous(breaks = c(-10,0, 10)) +
+                        facet_wrap(~genotype) + scale_y_continuous(breaks = c(-10,0, 10)) +
                         scale_x_continuous(breaks = c(-10,0, 10))+
                         theme(panel.spacing = unit(0.5, "lines"))+
                         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
@@ -25,17 +30,22 @@ b_all_plotlist <- map(.x = c("Cd19", "Pax5", "Cd24a", "Cd79a", "Vpreb1", "Vpreb2
                       p
                     })
 
-b_all_plotlist[[1]] / b_all_plotlist[[2]] |
-  b_all_plotlist[[3]] / b_all_plotlist[[4]]
+Supp_Fig_4A.1 <- b_plotlist[[1]] / b_plotlist[[2]] /
+  b_plotlist[[3]] / b_plotlist[[4]]
 
-b_all_plotlist[[5]] / b_all_plotlist[[6]] |
-  b_all_plotlist[[7]] / b_all_plotlist[[8]]
+Supp_Fig_4A.2 <- b_plotlist[[5]] / b_plotlist[[6]] /
+  b_plotlist[[7]] / b_plotlist[[8]]
 
+Supp_Fig_4A.1
+Supp_Fig_4A.2
 
-#bb_gene_umap(cds_main, gene_or_genes = c("Dntt"), alt_dim_x = "aggr_UMAP_1", alt_dim_y = "aggr_UMAP_2")+ facet_wrap(~leukemia_phenotype)
-
-t_all_plotlist <- map(.x = c("Atxn1", "Ly6a", "Cd7", "Cd3d"), #Atxn1 = Sca1
-                    .f = \(x, dat = cds_main) {
+#Supplemental Figure 4B
+t_plotlist <- map(.x = c("Atxn1", "Ly6a", "Cd7", "Cd3d"), #Atxn1 = Sca1
+                    .f = \(x, dat = filter_cds(
+                      cds_main,
+                      cells = bb_cellmeta(cds_main) |>
+                        filter(geno_pheno %in% c("dKO: AML", "Wildtype", "P53 KO: T ALL"))
+                    )) {
                       p <- bb_gene_umap(
                         dat,
                         gene_or_genes = x,
@@ -46,7 +56,7 @@ t_all_plotlist <- map(.x = c("Atxn1", "Ly6a", "Cd7", "Cd3d"), #Atxn1 = Sca1
                         scale_color_distiller(palette = "Blues",
                                               direction = 1,
                                               na.value = "grey80", limits = c(0,2)) + #check limits!
-                        facet_wrap(~geno_pheno) + scale_y_continuous(breaks = c(-10,0, 10)) +
+                        facet_wrap(~genotype) + scale_y_continuous(breaks = c(-10,0, 10)) +
                         scale_x_continuous(breaks = c(-10,0, 10))+
                         theme(panel.spacing = unit(0.5, "lines"))+
                         theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
@@ -59,5 +69,6 @@ t_all_plotlist <- map(.x = c("Atxn1", "Ly6a", "Cd7", "Cd3d"), #Atxn1 = Sca1
                       if (x != "Cd3d") p <- p + theme(legend.position = "none")
                       p
                     })
-t_all_plotlist[[1]] / t_all_plotlist[[2]] |
-  t_all_plotlist[[3]] / t_all_plotlist[[4]]
+Supp_Fig_4B <- t_plotlist[[1]] / t_plotlist[[2]] /
+  t_plotlist[[3]] / t_plotlist[[4]]
+Supp_Fig_4B
